2c1f9b32044bbb77f436e56ee88c3e8ab4a40081
Tim <81175852+wharvex@users.noreply.github.com>
2024-11-23 12:39:38 -0500

    refactor to avoid code duplication

--- src/Variable.java
+++ src/Variable.java
@@ -1,40 +1,6 @@
-public class Variable {
-    private final char symbol;
-
+public class Variable extends VariableOrTerminal {
     public Variable(char symbol) {
-        this.symbol = symbol;
+        super(symbol);
     }
 
-    /**
-     * https://stackoverflow.com/a/8180925/16458003
-     *
-     * @param obj
-     * @return
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-
-        if (obj.getClass() != this.getClass()) {
-            return false;
-        }
-
-        final Variable other = (Variable) obj;
-
-        return this.symbol == other.symbol;
-    }
-
-    /**
-     * https://stackoverflow.com/a/8180925/16458003
-     *
-     * @return
-     */
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 53 * hash + this.symbol;
-        return hash;
-    }
 }
---
+++ src/VariableOrTerminal.java
@@ -0,0 +1,40 @@
+public class VariableOrTerminal {
+    private final char symbol;
+
+    protected VariableOrTerminal(char symbol) {
+        this.symbol = symbol;
+    }
+
+    /**
+     * https://stackoverflow.com/a/8180925/16458003
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final VariableOrTerminal other = (VariableOrTerminal) obj;
+
+        return this.symbol == other.symbol;
+    }
+
+    /**
+     * https://stackoverflow.com/a/8180925/16458003
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.symbol;
+        return hash;
+    }
+}
