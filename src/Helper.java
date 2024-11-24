904640072160800d976bd59e4fec944c72d98e17
Siddghul <siddghul@gmail.com>
2024-11-24 08:36:30 -0500

    Getting Grammer in a Hash Table From File

--- src/Helper.java
+++ src/Helper.java
@@ -1,18 +1,49 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
+
 
 public class Helper {
-    public static String readFromFile(String fileName) throws IOException {
+
+    public static Hashtable readFromFile(String fileName) throws IOException {
+
+        Hashtable<Character, String> grammer = new Hashtable<>();
+
+        char temporaryTerminal = ' ';
+        char temporaryNonTerminal = ' ';
+
         var fileDir = System.getProperty("user.dir");
         System.out.println("Looking for " + fileName + " in " + fileDir + "...");
         var fr = new FileReader(fileName);
 
         // 'Try with resources' so we automatically dispose the BufferedReader.
         try (BufferedReader br = new BufferedReader(fr)) {
+
             var sb = new StringBuilder();
-            br.lines().forEach(l -> sb.append(l).append(System.lineSeparator()));
-            return sb.toString();
+            var rightSide = new StringBuilder();
+            br.lines().forEach(l -> sb.append(l.replace(" ", "")).append(System.lineSeparator()));
+            char[] eachLetter = sb.toString().toCharArray();
+            System.out.println(eachLetter[4]);
+
+            for(int i=0; i<eachLetter.length; i++) {
+                temporaryNonTerminal = eachLetter[i];
+                i++;
+                if(eachLetter[i] == '→') {
+                    i++;
+                    while(eachLetter[i] != '\r') {
+                        rightSide.append(eachLetter[i]);
+                        i++;
+                    }
+                    if(eachLetter[i] == '\r'){
+                        grammer.put(temporaryNonTerminal, rightSide.toString());
+                        temporaryNonTerminal = ' ';
+                        rightSide = new StringBuilder();
+                        i++;
+                    }
+                }
+            }
+            return grammer;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import java.io.IOException;
+import java.util.Hashtable;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(Helper.readFromFile("test.txt"));
+        Hashtable Grammer = Helper.readFromFile("test.txt");
+
+        System.out.println(Grammer);
     }
+
+
 }
