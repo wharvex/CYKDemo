8110c3b41ac6251f5cee8182b2e466d89ebb3600
Tim <81175852+wharvex@users.noreply.github.com>
2024-11-23 22:08:21 -0500

    make `CYKTable.getEntry` method, etc
    * Make `CYKTable.getEntry` method.
    
    * Change `CYKTableEntry.variables` to a set.
    
    * Override equals for `CYKTableEntry`.
    
    * Make `CYKTable` test class.

--- src/CYKTableEntry.java
+++ src/CYKTableEntry.java
@@ -1,14 +1,65 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CYKTableEntry {
     private final int i;
     private final int j;
-    private final List<Variable> variables;
+    private final Set<Variable> variables;
 
     public CYKTableEntry(int i, int j) {
         this.i = i;
         this.j = j;
-        this.variables = new ArrayList<>();
+        this.variables = new HashSet<>();
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    /**
+     * https://stackoverflow.com/a/8180925/16458003
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final CYKTableEntry other = (CYKTableEntry) obj;
+
+        return getI() == other.getI() && getJ() == other.getJ() && variables.equals(other.variables);
+    }
+
+    public void addVariable(Variable variable) {
+        variables.add(new Variable(variable));
+    }
+
+    public Set<TwoVariables> cartesianProduct(CYKTableEntry other) {
+        return null;
+    }
+
+    /**
+     * https://stackoverflow.com/a/8180925/16458003
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + getI();
+        hash = 53 * hash + getJ();
+        hash = 53 * hash + variables.stream().map(Variable::hashCode).reduce(0, Integer::sum);
+        return hash;
     }
 }
---
+++ test/CYKTableTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CYKTableTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testGetEntry() {
+    }
+}
